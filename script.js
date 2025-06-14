6ec3b37529b92faf326b4d615c29c8facf2a92f1
Akash <codesecoder@gmail.com>
2025-06-14 15:47:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+  function changeColor() {
+    const boxs = document.querySelectorAll('.topic1');
+    boxs.forEach(topic1 => {
+    topic1.classList.remove('bg-indigo-300');
+    topic1.classList.add('bg-red-500');
+  });
+}
+  function clickPre() {
+    window.open('https://sarkariresultclone.blogspot.com/', '_blank');
+}
